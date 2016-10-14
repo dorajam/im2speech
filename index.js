b60e795782271f0c274225d255cfa4e3331ac93b
dorajambor <dorajambor@gmail.com>
2016-10-14 13:47:45 +0200

    vid starts up, but doesnt show

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 
 var canvas = document.getElementById('canvas');
+var canvas2 = document.getElementById('canvas2');
 var context = canvas.getContext('2d');
+var context2 = canvas2.getContext('2d');
 var video = document.getElementById('video');
 
-// Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+navigator.mediaDevices.getUserMedia( {video: true})
+    .then((stream) => {
         video.src = window.URL.createObjectURL(stream);
         video.play();
-    });
-}
+    }).catch((err) => {console.log(err);});
+
 
 // Trigger photo take
 document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
+	context2.drawImage(video, 0, 0, 640, 480);
 });
+
+// creates new canvas element for the photo taken
+function convertImageToCanvas(image) {
+    var canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height
+    canvas.getContext("2d").drawImage(image,0,0);
+    return canvas;
+}
+
+function canvasToImage(canvas) {
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+    return image;
+}
